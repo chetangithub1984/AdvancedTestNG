f0fb43f82dfbf5b9d069395259075659394207a4
chetangithub1984 <chetan.capgemini@gmail.com>
2019-05-04 21:55:07 +0530

    added first and second prog

---
+++ src/test/java/com/chetan/advancedtestng/FirstProg.java
@@ -0,0 +1,14 @@
+package com.chetan.advancedtestng;
+
+import org.testng.annotations.Test;
+
+public class FirstProg {
+	
+	@Test
+	
+	public void firsttest() {
+		
+		System.out.println("first test started");
+	}
+
+}
---
+++ src/test/java/com/chetan/advancedtestng/SecondProg.java
@@ -0,0 +1,14 @@
+package com.chetan.advancedtestng;
+
+import org.testng.annotations.Test;
+
+public class SecondProg {
+	
+	@Test
+	
+	public void secondtest() {
+		
+		System.out.println("second test started");
+	}
+
+}
